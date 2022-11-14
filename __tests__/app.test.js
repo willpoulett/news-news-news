518158eee897657f0800f107e8e95082e6582dae
willpoulett <william.poulett1@nhs.net>
2022-11-14 16:49:30 +0000

    task 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -41,3 +41,41 @@ describe('/api/topics', () => {
         });
     });  
 });
+
+//-----------------------
+
+describe('/api/articles', () => {
+    describe('Functionality', () => {
+        test('GET 200: responds with an array of article objects, with date sorted in descending order', () => {
+            return request(app)
+            .get('/api/articles')
+            .expect(200)
+            .then( (res) => {
+                expect(res.body.articles).toEqual(expect.any(Array))
+                expect(res.body.articles).toBeSortedBy('created_at', {descending: true})
+                res.body.articles.forEach( (article) => {
+                    expect(article).toMatchObject({
+                        author: expect.any(String),
+                        title: expect.any(String),
+                        article_id: expect.any(Number),
+                        topic: expect.any(String),
+                        created_at: expect.any(String),
+                        votes: expect.any(Number),
+                        comment_count: expect.any(String),
+                    });
+                });
+            });
+        });
+    });
+
+    describe('Error handling', () => {
+        test('Error 404: responds with error message', () => {
+            return request(app)
+            .get('/api/articlesddss')
+            .expect(404)
+            .then( (res) => {
+                expect(res.body.msg).toBe('Not Found')
+            })
+        });
+    });  
+});
--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express")
-const {getTopics} = require('./controllers/news_site.js')
+const {getTopics, getArticles} = require('./controllers/news_site.js')
 const app = express()
 app.get('/api/topics',getTopics)
+
+app.get('/api/articles',getArticles)
+
 
 
 app.all('/*', (req,res) => {
     res.status(404).send({msg:'Not Found'})
 })
   
+app.use((err, req, res, next) => {
+    res.sendStatus(500)
+})
 module.exports = app
