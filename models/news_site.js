d2ff24c34e9ca53d4b7a0bb57d7701cd9bcb1d4d
willpoulett <william.poulett1@nhs.net>
2022-11-16 11:07:11 +0000

    comments removed

--- models/news_site.js
+++ models/news_site.js
@@ -88,27 +88,3 @@ exports.fetchCommentsByArticleId = (article_id) => {
     })
   };
 
-
-
-  // ______________________________________________________
-  
-
-  // const articleIsReal = async (article_id) => {
-  //   return db
-  //     .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
-  //     .then((queryOutput) => {
-  //       if (queryOutput.rows.length === 0) {
-  //         return Promise.reject({ status: 404, msg: "That article does not exist" });
-  //       }
-  //     });
-  // };
-
-  // const userExists = async (username) => {
-  //   return db
-  //     .query(`SELECT * FROM users WHERE username = $1`, [username])
-  //     .then((queryOutput) => {
-  //       if (queryOutput.rows.length === 0) {
-  //         return Promise.reject({ status: 401, msg: "User does not exist" });
-  //       }
-  //     });
-  // };
