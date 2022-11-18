d135e8529d8aaaba8850904596a37b4560f8e0fd
willpoulett <william.poulett1@nhs.net>
2022-11-18 11:30:30 +0000

    hosting

--- models/news_site.js
+++ models/news_site.js
@@ -71,12 +71,7 @@ exports.fetchArticleById = (article_id) => {
       JOIN comments
       ON comments.article_id = articles.article_id
       WHERE articles.article_id = $1
-      GROUP By articles.author,
-      articles.title,
-      articles.article_id,
-      articles.topic,
-      articles.created_at,
-      articles.votes;`, [article_id])
+      GROUP By articles.article_id;`, [article_id])
       .then((result) => {
         if (result.rows.length === 0){
           return Promise.reject({status:404,msg:'That article does not exist'})
